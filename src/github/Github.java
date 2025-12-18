3316e43ee9352dee0b40d303d12d6a664925b935
TOTAL TECH <TOTAL TECH@DESKTOP-G2NTLHJ>
2025-12-18 11:35:11 -0800

    Updated version!

--- src/github/Github.java
+++ src/github/Github.java
@@ -1,11 +1,10 @@
-
 package github;
-
 
 public class Github {
 
-   
     public static void main(String[] args) {
-        System.out.println("Hello world");    }
-    
+        System.out.println("Not");
+        System.out.println("Hello world");
+    }
+
 }
